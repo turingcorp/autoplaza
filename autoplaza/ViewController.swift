d10272c139c78e6ed2ba6d38b0fa46e2ad92faac
Zero <zero.griffin@gmail.com>
2018-03-18 09:59:37 +0100

    Initial Commit

---
+++ autoplaza/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  autoplaza
+//
+//  Created by zero on 18.03.18.
+//  Copyright © 2018 iturbide. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+

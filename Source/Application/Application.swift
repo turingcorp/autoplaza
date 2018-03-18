0952198e2a3194541da8b90b2c2e571215f09c9d
Zero <zero.griffin@gmail.com>
2018-03-18 10:20:30 +0100

    Create application

---
+++ Source/Application/Application.swift
@@ -0,0 +1,10 @@
+import UIKit
+
+@UIApplicationMain
+class Application:UIResponder, UIApplicationDelegate {
+    var window:UIWindow?
+    
+    func application(_:UIApplication, didFinishLaunchingWithOptions:[UIApplicationLaunchOptionsKey:Any]?) -> Bool {
+        return true
+    }
+}

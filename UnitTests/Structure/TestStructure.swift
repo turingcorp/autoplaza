dffd16a43dc4be6ca0446f53f72d5ec303e6360a
Zero <zero.griffin@gmail.com>
2018-03-18 11:14:27 +0100

    Create structure item protocol

--- UnitTests/Structure/TestStructure.swift
+++ UnitTests/Structure/TestStructure.swift
@@ -11,5 +11,11 @@ class TestStructure:XCTestCase {
     
     func testInit() {
         XCTAssertNotNil(self.structure, "Failed to load structure")
+        XCTAssertNotNil(self.structure?.items, "Structure has no items")
+    }
+    
+    func testFactory() {
+        let items:[StructureItemProtocol] = Structure.factoryItems()
+        XCTAssertGreaterThan(items.count, 0, "Factorised items is empty")
     }
 }

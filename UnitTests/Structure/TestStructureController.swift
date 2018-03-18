00a7649e8bb8e1370f07f9d6935061ece6922140
Zero <zero.griffin@gmail.com>
2018-03-18 11:33:34 +0100

    Add test for controller

---
+++ UnitTests/Structure/TestStructureController.swift
@@ -0,0 +1,15 @@
+import XCTest
+@testable import autoplaza
+
+class TestStructureController:XCTestCase {
+    private var controller:StructureController?
+    
+    override func setUp() {
+        super.setUp()
+        self.controller = StructureController()
+    }
+    
+    func testInit() {
+        XCTAssertNotNil(self.controller, "Failed to load controller")
+    }
+}

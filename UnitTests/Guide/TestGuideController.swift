a629e78489bc46c8d34da81101252821bd3047da
Zero <zero.griffin@gmail.com>
2018-03-19 08:00:15 +0100

    Create test for guide controller

---
+++ UnitTests/Guide/TestGuideController.swift
@@ -0,0 +1,16 @@
+import XCTest
+@testable import autoplaza
+
+class TestGuideController: XCTestCase {
+    private var controller:GuideController!
+    
+    override func setUp() {
+        super.setUp()
+        self.controller - GuideController()
+    }
+    
+    func testInit() {
+        XCTAssertNotNil(self.controller, "Failed to load controller")
+    }
+    
+}

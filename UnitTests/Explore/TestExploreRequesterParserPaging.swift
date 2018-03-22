c7985dbac27522abdf97cee590c56756f50949c6
Zero <zero.griffin@gmail.com>
2018-03-22 09:28:20 +0100

    Add tests for paging parser

--- UnitTests/Explore/TestExploreRequesterParserPaging.swift
+++ UnitTests/Explore/TestExploreRequesterParserPaging.swift
@@ -3,13 +3,54 @@ import XCTest
 
 class TestExploreRequesterParserPaging:XCTestCase {
     private var parser:ExploreRequesterParserPaging!
+    private var jsonSuccess:Data!
+    private var jsonError:Data!
+    private var dictionarySuccess:[String:Any]!
+    private var dictionaryError:[String:Any]!
+    private struct Constants {
+        static let total:Int = 110690
+        static let offset:Int = 33
+        static let limit:Int = 1
+    }
     
     override func setUp() {
         super.setUp()
+        let jsonParser:ExploreRequesterParser = ExploreRequesterParser()
         self.parser = ExploreRequesterParserPaging()
+        self.jsonSuccess = JsonMocker.paging01
+        self.jsonError = JsonMocker.pagingError01
+        
+        do {
+            try self.dictionarySuccess = jsonParser.getJsonFrom(data:self.jsonSuccess)
+            try self.dictionaryError = jsonParser.getJsonFrom(data:self.jsonError)
+        } catch {}
     }
     
     func testInit() {
         XCTAssertNotNil(self.parser, "Failed to load parser")
+        XCTAssertNotNil(self.jsonSuccess, "Failed to load json success")
+        XCTAssertNotNil(self.jsonError, "Failed to load json error")
+        XCTAssertNotNil(self.dictionarySuccess, "Failed to load dictionary success")
+        XCTAssertNotNil(self.dictionaryError, "Failed to load dictionary error")
+    }
+    
+    func testThrowErrorOnWrongJson() {
+        XCTAssertThrowsError(try self.parser.parse(json:self.dictionaryError))
+    }
+    
+    func testNotThrowOnSuccess() {
+        XCTAssertNoThrow(try self.parser.parse(json:self.dictionarySuccess))
+    }
+    
+    func testParsingValues() {
+        let paging:SearchPaging
+        do {
+            try paging = self.parser.parse(json:self.dictionarySuccess)
+        } catch {
+            return
+        }
+        XCTAssertEqual(paging.total, Constants.total, "Total not parsed properly")
+        XCTAssertEqual(paging.limit, Constants.limit, "Limit not parsed properly")
+        XCTAssertEqual(paging.offset, Constants.offset, "Offset not parsed properly")
     }
 }
--- UnitTests/Json/JSonMocker.swift
+++ UnitTests/Json/JSonMocker.swift
@@ -4,12 +4,16 @@ struct JsonMocker {
     static let requestSearch01:Data? = factoryJson(name:Constants.requestSearch01)
     static let empty:Data? = factoryJson(name:Constants.empty)
     static let emptyDictionary:Data? = factoryJson(name:Constants.emptyDictionary)
+    static let paging01:Data? = factoryJson(name:Constants.paging01)
+    static let pagingError01:Data? = factoryJson(name:Constants.pagingError01)
     
     private struct Constants {
         static let fileExtension:String = "json"
         static let requestSearch01:String = "JsonRequestSearch01"
         static let empty:String = "JsonEmpty"
         static let emptyDictionary:String = "JsonEmptyDictionary"
+        static let paging01:String = "JsonPaging01"
+        static let pagingError01:String = "JsonPagingError01"
     }
     
     private static func factoryJson(name:String) -> Data? {
